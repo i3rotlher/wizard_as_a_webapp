f446921ca621ff916f53ba5c0caec8876f42381e
nstre <63291884+nstre@users.noreply.github.com>
2022-11-14 11:14:56 +0100

    move rest to external js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -13,3 +13,65 @@ function play() {
         window.location.replace(res.url);
     });
 }
+
+function sendPlayerCount(amount) {
+    fetch(`http://localhost:9000/playerCount?count=${amount}`, {
+        method: "POST",
+        body: ""
+    }).then(res => {
+        window.location.replace(res.url);
+
+    });
+}
+
+function setName(name) {
+    if (this.event.key === "Enter") {
+        fetch(`http://localhost:9000/playerName?name=${name}`, {
+            method: "POST",
+            body: ""
+        }).then(res => {
+            if(res.redirected) {
+                window.location.replace(res.url)
+            } else {
+                window.location.replace(`http://localhost:9000/playerName`);
+            }
+        });
+    }
+}
+
+function playCard(idx) {
+    fetch(`http://localhost:9000/playCard?idx=${idx}`, {
+        method: "POST",
+        body: "",
+    }).then((res) => {
+        window.location.replace(res.url);
+    });
+}
+
+function setTricks(tricks) {
+    if (this.event.key === "Enter") {
+        fetch(`http://localhost:9000/setTrickAmount?amount=${tricks}`, {
+            method: "POST",
+            body: "",
+        }).then((res) => {
+            if (res.redirected) {
+                window.location.replace(res.url);
+            } else {
+                window.location.replace(`http://localhost:9000/setTrickAmount`);
+            }
+        });
+    }
+}
+
+function proceed() {
+    window.location.replace(`http://localhost:9000/setTrickAmount`)
+}
+
+function setTrump(color) {
+    fetch(`http://localhost:9000/trump?color=${color}`, {
+        method: "POST",
+        body: ""
+    }).then(res => {
+        window.location.replace(res.url)
+    });
+}
